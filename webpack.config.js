feb3b9c8b4a0e2c247170ff05374c72f81aebe5a
Kamran Ahmed <kamranahmed.se@gmail.com>
2017-08-11 02:33:05 +0400

    Add webpack and basic configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: './build',
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
   }
 };
